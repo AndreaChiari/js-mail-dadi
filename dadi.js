9fb33432dfeb47c5cfa8f33443b17a244e5ee701
AndreaChiari <chiariandrea94@gmail.com>
2022-12-12 15:42:24 +0100

    esercizio-completo-button-added

--- dadi.js
+++ dadi.js
@@ -9,30 +9,38 @@ console.log('JS OK')
 -stampo in pagina il risultato
 */
 
-const randomNumber1=Math.floor(Math.random()*6)+1;
-const randomNumber2=Math.floor(Math.random()*6)+1;
-// genero un numero da 1 a 6 per ogni sfidante
 button.addEventListener("click",function(){
-    randomNumber1
+
+//    genero un numero random da 1 a 6 per ogni giocatore
+
+    const randomNumber1=Math.floor(Math.random()*6)+1;
+    const randomNumber2=Math.floor(Math.random()*6)+1;
+
+//    assegno i numeri ad ogni sfidante
+
+    const number1=("PLAYER 1 : " + randomNumber1);
+    const number2=("PLAYER 2 : " + randomNumber2);
+    console.log(number1 , number2);
+
+ //   stampo in pagina i tiri
+
+    document.getElementById("player1").innerText = (number1);  
+    document.getElementById("player2").innerText = (number2); 
+
+ //   applico una condizionale che indica il vincitore
+
+    if(randomNumber1 > randomNumber2 ){
+        document.getElementById("result").innerText = ("THE WINNER IS: PLAYER 1 !!!" );
+        
+    }else if(randomNumber1 === randomNumber2){
+        document.getElementById("result").innerText = ("DRAW!!! REROLL PLEASE!" );
+    
+    }else if(randomNumber1 < randomNumber2 ){
+        document.getElementById("result").innerText = ("THE WINNER IS: PLAYER 2 !!!" );
+        
+    }
+
 })
 
-// assegno ad ogni sfidante un numero
-const number1=("player1 " + randomNumber1);
-const number2=("player2 " + randomNumber2);
-console.log(number1 , number2);
 
-// stampo in pagina il numero di ciancuno sfidante
-document.getElementById("player1").innerText = (number1);  
-document.getElementById("player2").innerText = (number2); 
 
-// creo la condizione che mi paermette di capire chi sara' il vincitore
-if(randomNumber1 > randomNumber2 ){
-    document.getElementById("result").innerText = ("THE WINNER IS: PLAYER 1 !!!" );
-    
-}else if(randomNumber1 === randomNumber2){
-    document.getElementById("result").innerText = ("DRAW!!! REROLL PLEASE!" );
-
-}else if(randomNumber1 < randomNumber2 ){
-    document.getElementById("result").innerText = ("THE WINNER IS: PLAYER 2 !!!" );
-    
-}
