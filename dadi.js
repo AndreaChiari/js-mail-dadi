7b5fa894421f6b7f164d123a10279809251b5495
AndreaChiari <chiariandrea94@gmail.com>
2022-12-12 15:03:34 +0100

    esercizio-completo-try-add-button

--- dadi.js
+++ dadi.js
@@ -9,13 +9,12 @@ console.log('JS OK')
 -stampo in pagina il risultato
 */
 
-
-const result= document.getElementById("result");
+const randomNumber1=Math.floor(Math.random()*6)+1;
+const randomNumber2=Math.floor(Math.random()*6)+1;
 // genero un numero da 1 a 6 per ogni sfidante
-
-let randomNumber1=Math.floor(Math.random()*6)+1;
-let randomNumber2=Math.floor(Math.random()*6)+1;
-
+button.addEventListener("click",function(){
+    randomNumber1
+})
 
 // assegno ad ogni sfidante un numero
 const number1=("player1 " + randomNumber1);
@@ -26,11 +25,14 @@ console.log(number1 , number2);
 document.getElementById("player1").innerText = (number1);  
 document.getElementById("player2").innerText = (number2); 
 
-// credo la condizione che mi paermette di capire chi sara' il vincitore
-if(number1 > number2 ){
-
-    document.getElementById("result").innerText = ('THE WINNER IS: ' + number1);
+// creo la condizione che mi paermette di capire chi sara' il vincitore
+if(randomNumber1 > randomNumber2 ){
+    document.getElementById("result").innerText = ("THE WINNER IS: PLAYER 1 !!!" );
     
-}else if(number1 == number2){
+}else if(randomNumber1 === randomNumber2){
+    document.getElementById("result").innerText = ("DRAW!!! REROLL PLEASE!" );
 
+}else if(randomNumber1 < randomNumber2 ){
+    document.getElementById("result").innerText = ("THE WINNER IS: PLAYER 2 !!!" );
+    
 }
