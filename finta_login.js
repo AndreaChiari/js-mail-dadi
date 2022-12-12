84c851db569634549b33a4ef8bba064293e18947
AndreaChiari <chiariandrea94@gmail.com>
2022-12-12 16:51:32 +0100

    update-logic-adjust

--- finta_login.js
+++ finta_login.js
@@ -2,20 +2,25 @@ console.log('JS OK')
 
 /*
 1) creo un array di email
-2) cero un prompt nel quale chiedo all'utente la sua e mail
-3)verifico se la mail corrisponde nell'elenco
-4)stampo un messaggio sull'esito del controllo
+2) prendo la mail dall'input 
+3) verifico se la mail corrisponde nell'elenco
+4) stampo un messaggio sull'esito del controllo
 */
 
 const RPGmail = ["mage@gmail.com","warrior@gmail.com","thief@gmail.com","healer@gmail.com" ];
+const button = document.getElementById('button');
 
-const questionMail = prompt('Hello hero ! please enter your E-mail');
+button.addEventListener("click",function(){
 
-if(questionMail === RPGmail){
-    document.getElementById("authorize").innerText=('ACCESS ALLOWED ! WELCOME TO THIS LAND !')
-    h1.classList.add("text-success");
+   
+    const inputMail = document.getElementById('input').value;
 
-}else if (questionMail !== RPGmail){
+    if(inputMail === RPGmail[0]){
+        document.getElementById("authorize").innerText=('ACCESS ALLOWED ! WELCOME TO THIS LAND !')
+        document.getElementById("authorize").classList.add("text-success");
+        
+    }else if (inputMail !== RPGmail){
     document.getElementById("authorize").innerText=('ACCESS DENIED ! GO HOME OUTLANDER !')
-    h1.classList.add("text-danger");
-}
+    document.getElementById("authorize").classList.add("text-danger");
+}})
+
